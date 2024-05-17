78557bd49ecf344c9add225814cae1dae4f1aacd
Jon Heinritz <jon.heinritz@protonmail.com>
2024-05-17 22:47:07 +0200

    feat(treesitter): configure the nvim-treesitter plugin

---
+++ lua/custom/plugins/treesitter.lua
@@ -0,0 +1,35 @@
+---@type LazyPluginSpec
+local M = {
+  "nvim-treesitter/nvim-treesitter",
+  event = "BufReadPost",
+  dependencies = {
+    "nvim-treesitter/playground",
+    "nvim-tree/nvim-web-devicons",
+  },
+}
+
+function M.config()
+  require "nvim-treesitter.configs".setup {
+    modules = {},
+    ensure_installed = {},
+    ignore_install = {},
+
+    sync_install = false,
+    auto_install = true,
+
+    highlight = { enable = true },
+    matchup = { enable = true },
+    ident = { enable = true },
+    incremental_selection = { enable = true },
+
+    playground = { enable = true },
+
+    query_linter = {
+      enable = true,
+      use_virtual_text = true,
+      lint_events = { "BufWrite", "CursorHold" },
+    },
+  }
+end
+
+return M
