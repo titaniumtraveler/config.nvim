461b3b4c0e307f14ce003705a4b9584f12f8f133
Jon Heinritz <jon.heinritz@protonmail.com>
2024-05-18 06:41:08 +0200

    feat(completion): add vim_dadbod completion

--- lua/custom/plugins/vim_dadbod.lua
+++ lua/custom/plugins/vim_dadbod.lua
@@ -2,8 +2,14 @@
 local M = {
   "kristijanhusak/vim-dadbod-ui",
   dependencies = {
-    { "tpope/vim-dadbod", lazy = true },
-    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    {
+      "kristijanhusak/vim-dadbod-completion",
+      ft = { "sql", "mysql", "plsql" },
+      dependencies = {
+        "hrsh7th/nvim-cmp",
+        "tpope/vim-dadbod",
+      },
+    },
   },
   cmd = {
     "DBUI",
@@ -13,6 +19,13 @@ local M = {
   },
   init = function()
     vim.g.db_ui_use_nerd_fonts = 1
+
+    require "cmp".setup.filetype({ "sql", "mysql", "plsql" }, {
+      sources = {
+        { name = "vim-dadbod-completion" },
+        { name = "buffer" },
+      },
+    })
   end,
 }
 
