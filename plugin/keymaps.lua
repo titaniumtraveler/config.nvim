7c521e9d7a1a138b556ee4c1e52053658a2a77dc
Jon Heinritz <jon.heinritz@protonmail.com>
2024-05-24 06:08:38 +0200

    fix(keymaps): remove `<c-k>` for `<Esc>` mapping as I don't use it anyway

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -1,7 +1,5 @@
 vim.keymap.set({ "n", "i", "c", "v", "x", "s", "o", "l" }, "<C-j>", "<Esc>", { remap = true })
-vim.keymap.set({ "n", "i", "c", "v", "x", "s", "o", "l" }, "<C-k>", "<Esc>", { remap = true })
 vim.keymap.set("t", "<C-j>", "<C-\\><C-n>", { remap = true })
-vim.keymap.set("t", "<C-k>", "<C-\\><C-n>", { remap = true })
 
 vim.keymap.set({ "n", "i", "c" }, "<C-l>", "<CR>", { remap = true })
 
