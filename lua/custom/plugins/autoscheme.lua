13fc82cb0efda13bdf6a2991265a8c26a846f546
Jon Heinritz <jon.heinritz@protonmail.com>
2024-07-04 14:30:13 +0200

    feat(colorscheme): use base16.toml in config instead of a symlink

--- lua/custom/plugins/autoscheme.lua
+++ lua/custom/plugins/autoscheme.lua
@@ -7,9 +7,6 @@ local M = {
 }
 
 function M.config()
-  -- Note that this assumes that a file called `base16.toml` exists in `vim.fn.stdpath "config"`
-  -- This file comes from <https://github.com/titaniumtraveler/base16-nvim.toml>
-  -- and I usually just symlink it here. (Though I might change that in the future.)
   require "autoscheme".setup { "base16.toml" }
 
   vim.cmd.colorscheme "base16"
